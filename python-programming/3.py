107b108997dc568033bfa7c96ef91fad4adbc23d
spyc-banda <spycbanda@gmail.com>
2017-07-16 18:39:14 +0530

    Add files via upload

---
+++ python-programming/3.py
@@ -0,0 +1,7 @@
+name = input("Enter your name")
+print(name)
+num1 = int( input("Enter number 1") )
+num2 = int( input("Enter number 2") )
+num3 = int( input("Enter number 3") )
+average=(num1+num2+num3)/3
+print("average of entered values is", average)
