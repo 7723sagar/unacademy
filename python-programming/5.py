eac6cb700a8736a1177d18f8d6b13c573c231b9c
spyc-banda <spycbanda@gmail.com>
2017-07-24 04:02:36 +0530

    Add files via upload

---
+++ python-programming/5.py
@@ -0,0 +1,13 @@
+print("hello "+"world!")
+print("Hey "*2)
+print("Hey "*3+"dude")
+print("%s to the %s of %s" %("welcome",'world',"Python"))
+
+print("yo are crazy and ''\n","crazy" and "")
+print("hello" or "yellow")
+
+print("Is 'c' present in 'call'?",'c' in "call")
+print("Is not 'ant' present in 'antonio'?", 'ant' in "antonio")
+
+print("%s is %s till you %s us"%("welcome","welcome","welcome"))
+print("\n \n \nBye Student")
