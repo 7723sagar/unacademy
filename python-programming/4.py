7b89670781ed865748b49c0d6f83aab31b902449
spyc-banda <spycbanda@gmail.com>
2017-07-23 16:04:27 +0530

    Add files via upload

---
+++ python-programming/4.py
@@ -0,0 +1,13 @@
+string = "I Anm Htejrle Tfo Haexlrp Yioque"
+print(string[3:15])
+print(string[3:15:1])
+print(string[0:len(string):2])
+print(string[::])
+print(string[::2])
+print()
+print(string[-1*len(string)+3:-1*len(string)+15:1])
+print(string[-1:-1*len(string):-1])
+print(string[::-1])
+
+
+
